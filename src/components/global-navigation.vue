ca2383d1ac435c7331e42f9df8bf789db2956124
林岑影 <lincenying@qq.com>
2023-03-10 01:21:26 +0800

    commit on 2023-03-10 01:21:26

--- src/components/global-navigation.vue
+++ src/components/global-navigation.vue
@@ -6,28 +6,33 @@
                     ><i class="icon icon-nav-logo"></i><span class="hidden">M.M.F 小屋</span></router-link
                 >
                 <div class="main-nav">
-                    <router-link to="/" active-class="current" exact class="nav-link"
-                        ><i class="icon icon-nav-home"></i><span class="text">首页</span></router-link
-                    >
-                    <router-link to="/trending/visit" active-class="current" class="nav-link"
-                        ><i class="icon icon-nav-explore"></i><span class="text">热门</span></router-link
-                    >
-                    <router-link to="/about" active-class="current" class="nav-link"
-                        ><i class="icon icon-nav-features"></i><span class="text">关于</span></router-link
-                    >
+                    <router-link to="/" active-class="current" exact class="nav-link">
+                        <i class="icon icon-nav-home"></i><span class="text">首页</span>
+                    </router-link>
+                    <router-link to="/trending/visit" active-class="current" class="nav-link">
+                        <i class="icon icon-nav-explore"></i><span class="text">热门</span>
+                    </router-link>
+                    <router-link to="/about" active-class="current" class="nav-link">
+                        <i class="icon icon-nav-features"></i><span class="text">关于</span>
+                    </router-link>
+                    <a href="https://handle.mmxiaowu.com/" target="_blank" class="nav-link">
+                        <i class="icon icon-nav-game"></i><span class="text">汉兜</span>
+                    </a>
                 </div>
             </div>
             <div v-if="!backend" class="right-part">
-                <span class="nav-search"
-                    ><i class="icon icon-search-white"></i><input @keyup.enter="onSearch($event)" placeholder="记得按回车哦" class="nav-search-input"
-                /></span>
-                <span v-if="isLogin" class="nav-me"
-                    ><router-link to="/user/account" class="nav-me-link"><img :src="$filters.avatar(userEmail)" class="nav-avatar-img" /></router-link
-                ></span>
-                <span v-else class="nav-me"
-                    ><a @click="handleLogin" href="javascript:;" class="nav-me-link"
-                        ><img :src="$filters.avatar('noavatar')" class="nav-avatar-img" /></a
-                ></span>
+                <span class="nav-search">
+                    <i class="icon icon-search-white"></i>
+                    <input @keyup.enter="onSearch($event)" placeholder="记得按回车哦" class="nav-search-input" />
+                </span>
+                <span v-if="isLogin" class="nav-me">
+                    <router-link to="/user/account" class="nav-me-link"><img :src="$filters.avatar(userEmail)" class="nav-avatar-img" /></router-link>
+                </span>
+                <span v-else class="nav-me">
+                    <a @click="handleLogin" href="javascript:;" class="nav-me-link">
+                        <img :src="$filters.avatar('noavatar')" class="nav-avatar-img" />
+                    </a>
+                </span>
             </div>
         </div>
     </nav>
--- vite.config.js
+++ vite.config.js
@@ -58,40 +58,6 @@ export default ({ mode }) => {
                     }
                 }
             }),
-            // styleImport({
-            //     libs: [
-            //         {
-            //             libraryName: 'ant-design-vue',
-            //             esModule: true,
-            //             resolveStyle: name => {
-            //                 return `ant-design-vue/es/${name}/style/index`
-            //             }
-            //         },
-            //         {
-            //             libraryName: 'antd',
-            //             esModule: true,
-            //             resolveStyle: name => {
-            //                 return `antd/es/${name}/style/index`
-            //             }
-            //         },
-            //         {
-            //             libraryName: 'vant',
-            //             esModule: true,
-            //             resolveStyle: name => {
-            //                 return `vant/es/${name}/style/index`
-            //             }
-            //         },
-            //         {
-            //             libraryName: 'element-plus',
-            //             resolveStyle: name => {
-            //                 return `element-plus/lib/theme-chalk/${name}.css`
-            //             },
-            //             resolveComponent: name => {
-            //                 return `element-plus/lib/${name}`
-            //             }
-            //         }
-            //     ]
-            // }),
             WindiCSS({
                 scan: {
                     dirs: ['.'], // all files in the cwd
