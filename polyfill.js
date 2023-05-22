221a3f2542f5fc0f8ddab84f3866953aa8ec4586
林岑影 <lincenying@qq.com>
2023-05-22 17:55:00 +0800

    feat: add polyfill

---
+++ polyfill.js
@@ -0,0 +1,21 @@
+import fs from 'node:fs'
+import polyfillLibrary from 'polyfill-library'
+
+polyfillLibrary.getPolyfillString({
+    uaString: 'Chrome/63',
+    minify: true,
+    features: {
+        es2015: { flags: ['gated'] },
+        es2016: { flags: ['gated'] },
+        es2017: { flags: ['gated'] },
+        es2018: { flags: ['gated'] },
+        es2019: { flags: ['gated'] },
+        es2020: { flags: ['gated'] },
+        es2021: { flags: ['gated'] },
+        es2022: { flags: ['gated'] },
+        fetch: { flags: ['gated'] },
+    },
+    excludes: ['AggregateError'],
+}).then((bundleString) => {
+    fs.writeFileSync('./public/static/js/polyfill.js', bundleString)
+})
