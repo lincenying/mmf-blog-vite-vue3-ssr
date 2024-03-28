d1b67a027405980dcce863f317ff5073cbfc1f78
林岑影 <lincenying@qq.com>
2024-03-28 15:36:00 +0800

    chore: build

--- dist/server.js
+++ dist/server.js
@@ -51,8 +51,8 @@ async function createServer() {
       index: false
     })
   );
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ limit: "50mb", extended: true }));
   app.use(cookieParser());
   app.use("*", async (req, res) => {
     try {
