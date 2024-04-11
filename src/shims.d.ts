4ab59c7b0fc09c45bd45790e87c41e0e4d08a6e5
林岑影 <lincenying@qq.com>
2024-04-11 23:21:33 +0800

    chore: update

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -2,3 +2,10 @@ declare module '@kangc/v-md-editor'
 declare module '@kangc/v-md-editor/lib/preview'
 declare module '@kangc/v-md-editor/lib/theme/vuepress'
 declare module '@kangc/v-md-editor/lib/plugins/line-number/index'
+
+interface ImportMetaEnv {
+    readonly VITE_APP_ENV: 'development' | 'production' | 'pre-release'
+    readonly VITE_APP_API_DOMAIN: string
+    readonly VITE_APP_API: string
+    // 更多环境变量...
+}
