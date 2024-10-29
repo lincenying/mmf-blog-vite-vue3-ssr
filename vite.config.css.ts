92d3cbaf8a7e03c525a99a85e112d80b08b4b8f0
林岑影 <lincenying@qq.com>
2024-10-29 17:25:40 +0800

    chore: 添加scss-silenceDeprecations配置

--- vite.config.css.ts
+++ vite.config.css.ts
@@ -18,6 +18,8 @@ const config: CSSOptions = {
     preprocessorOptions: {
         scss: {
             api: 'modern-compiler',
+            // 忽略scss global-builtin, import 提示3.0将删除的警告
+            silenceDeprecations: ['global-builtin', 'import'],
         },
     },
 }
