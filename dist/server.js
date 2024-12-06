03e4b7920d07b326fb7e928941fd8bb726b17479
林岑影 <lincenying@qq.com>
2024-12-06 16:04:27 +0800

    chore: update deps

--- dist/server.js
+++ dist/server.js
@@ -14,6 +14,7 @@ import serveStatic from "serve-static";
 // src/api/url.js
 import process from "node:process";
 var url = process.env.API_URL || "http://127.0.0.1:4000";
+console.log(`\u5F53\u524DAPI\u5730\u5740: ${url}`);
 var url_default = url;
 
 // server.prod.ts
